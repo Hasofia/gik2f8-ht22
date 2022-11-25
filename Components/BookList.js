ec3e847abe48fc36556c023c60e364597306ed03
Sofia Hagberg <sofia_hagberg@hotmail.se>
2022-11-25 16:49:08 +0100

    Lade till kommentarer

--- Components/BookList.js
+++ Components/BookList.js
@@ -1,5 +1,7 @@
+// Filnamnet döps på samma sätt som funktionen. Detta är en namnkonvention som är vanlig kring komponenter/klasser inom JS
+
 const BookList = (bookList) => {
-    let html = `<ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">`;
+    let html = `<ul class="book-list rounded-md border-2 border-blue-900 bg-white w-full mx-auto">`;
     for (let i = 0; i < bookList.length; i++) {
       html += BookListItem(bookList[i]);
     }
--- Components/BookListItem.js
+++ Components/BookListItem.js
@@ -1,3 +1,6 @@
+// Filnamnet döps på samma sätt som funktionen. Detta är en namnkonvention som är vanlig kring komponenter/klasser inom JS
+// "Book" då vi pratar om endast en enskild bok. Körs för varje bok i listan.
+
 const BookListItem = (book) => {
     let html = `<li
                   class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
